f470757f63e05263e4a4c82c245a6bc726fd5fa2
jeanl <jeanl@LAPTOP-OPUPP5I2>
2021-10-04 09:55:30 +0200

    création du depôt

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,33 @@
+/*
+ * Monnier Ambre et Lemaire Jean, TDA2, TP0, 29/09/2021
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jeanl
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prénom ?");
+        prenom = sc.nextLine();
+        
+        System.out.println("au revoir");
+        
+        
+    }
+    
+}
+
